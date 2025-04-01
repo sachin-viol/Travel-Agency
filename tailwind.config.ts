f29bb1cb9ba1f23b3dc9ba2c21db951146e9435d
sachin-viol <sachingupta.viol@gmail.com>
2025-04-01 19:59:20 +0530

    feat: integrate Tailwind CSS and enhance styling with custom utility classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -129,4 +128,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
