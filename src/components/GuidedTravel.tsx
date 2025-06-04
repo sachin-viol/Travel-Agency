0aa7a07012bf9001141c11f3f56b9b46001f7b0a
sachin-viol <sachingupta.viol@gmail.com>
2025-06-04 20:34:25 +0530

    feat: add new images for Guided Travel component and update text styles

--- src/components/GuidedTravel.tsx
+++ src/components/GuidedTravel.tsx
@@ -35,10 +35,10 @@ const GuidedTravel = () => {
             <h2 className="text-base/7 font-semibold text-travel-terracotta uppercase tracking-wider">
               Why travel with us
             </h2>
-            <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-4xl">
+            <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-travel-earth-light sm:text-5xl lg:text-4xl">
               Why Guided Travel by WANDER TRIBE
             </p>
-            <p className="mt-6 text-lg/8 text-gray-600">
+            <p className="mt-6 text-lg/8 text-slate-600">
               You're a traveler with places to go. We'll take care of the
               details. You embrace the journey.
             </p>
@@ -46,103 +46,51 @@ const GuidedTravel = () => {
           <div className="mx-auto mt-12 max-w-2xl sm:mt-12 lg:mt-16 lg:max-w-6xl">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
               <div className="relative pl-16">
-                <dt className="text-base/7 font-semibold text-gray-900">
+                <dt className="text-base/7 font-semibold text-travel-earth-light">
                   <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-travel-earth">
-                    <svg
-                      className="size-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
-                      />
-                    </svg>
+                    <img src="/network.png" alt="" className="size-8 text-white" />
                   </div>
                   Social Connections
                 </dt>
-                <dd className="mt-2 text-base/7 text-gray-600">
+                <dd className="mt-2 text-base/7 text-slate-600">
                   Meet a group of people who share your love of travel. Learn
                   from locals. Forge friendships in the coolest places.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base/7 font-semibold text-gray-900">
+                <dt className="text-base/7 font-semibold text-travel-earth-light">
                   <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-travel-earth">
-                    <svg
-                      className="size-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-                      />
-                    </svg>
+                    <img src="/freedom.png" alt="" className="size-8 text-white" />
                   </div>
                   Independence
                 </dt>
-                <dd className="mt-2 text-base/7 text-gray-600">
+                <dd className="mt-2 text-base/7 text-slate-600">
                   There's plenty of time built into our itineraries, plus
                   optional tours, Choice on Tour excursions, and more. It's your
                   trip, your way.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base/7 font-semibold text-gray-900">
+                <dt className="text-base/7 font-semibold text-travel-earth-light">
                   <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-travel-earth">
-                    <svg
-                      className="size-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                      />
-                    </svg>
+                    <img src="/planning.png" alt="" className="size-8 text-white" />
                   </div>
                   Expertly Planned
                 </dt>
-                <dd className="mt-2 text-base/7 text-gray-600">
+                <dd className="mt-2 text-base/7 text-slate-600">
                   You know what you want to see and do - let a team of experts
                   handle all the details from the must-sees to centrally-located
                   accommodations and local lessons.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base/7 font-semibold text-gray-900">
+                <dt className="text-base/7 font-semibold text-travel-earth-light">
                   <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-travel-earth">
-                    <svg
-                      className="size-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon">
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
-                      />
-                    </svg>
+                    <img src="/step-forward.png" alt="" className="size-8 text-white" />
                   </div>
                   The Best Pacing
                 </dt>
-                <dd className="mt-2 text-base/7 text-gray-600">
+                <dd className="mt-2 text-base/7 text-slate-600">
                   Don't rush – this is your time to see the world. Get more time
                   in the destination, and enjoy an ideal balance of included
                   excursions and free time.
